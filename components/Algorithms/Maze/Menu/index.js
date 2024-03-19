8c5aff41cf5dbc0ba362745d99a648415eb26796
yazloysasha <yazloysasha@gmail.com>
2024-03-19 13:49:30 +0700

    Добавлен инпут

--- components/Algorithms/Maze/Menu/index.js
+++ components/Algorithms/Maze/Menu/index.js
@@ -1,3 +1,4 @@
+import Input from "../../../Input";
 import Button from "../../../Button";
 import style from "./style.module.scss";
 
@@ -6,6 +7,14 @@ export default function Menu({
     setCount,
     refreshField
 }) {
+    const changeCountEvent = (event) => {
+        const value = event.target.value;
+
+        if (/^1?\d{0,2}$/.test(value)) {
+            setCount(Math.max(value, 0));
+        }
+    };
+
     return (
         <div className={style.menu}>
             <p className={style.title}>А*</p>
@@ -13,7 +22,18 @@ export default function Menu({
                 Сейчас вы узрите, как работает алгоритм нахождения пути в лабиринте.
             </span>
 
-            <div className={style.container}>
+            <div className={style.inputContainer}>
+                <Input
+                    type="text"
+                    label="Размеры поля"
+                    description="Введите размеры поля"
+                    maxLength={3}
+                    value={count}
+                    onChange={changeCountEvent}
+                />
+            </div>
+
+            <div className={style.buttonContainer}>
                 <Button type="primary">Запустить</Button>
                 <Button type="soft" onClick={refreshField}>
                     Перезагрузить
---
+++ components/Input/index.js
@@ -0,0 +1,25 @@
+import style from "./style.module.scss";
+
+export default function Input({
+    type,
+    label,
+    placeholder,
+    maxLength,
+    value,
+    disabled,
+    onChange
+}) {
+    return (
+        <div className={style.input}>
+            <label>{label}</label>
+            <input
+                type={type}
+                placeholder={placeholder}
+                maxLength={maxLength}
+                value={value}
+                disabled={disabled}
+                onChange={onChange}
+            />
+        </div>
+    );
+}
