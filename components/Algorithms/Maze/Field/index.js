c9491bfc93472ef546f47d4019777734d5be2ce7
yazloysasha <yazloysasha@gmail.com>
2024-03-20 17:12:08 +0700

    Начато изменение на Canvas

--- components/Algorithms/Maze/Field/index.js
+++ components/Algorithms/Maze/Field/index.js
@@ -1,5 +1,6 @@
-import Cell from "./Cell";
 import style from "./style.module.scss";
+import { useRef, useState, useEffect } from "react";
+import { createCanvas } from "../../../../utils/canvas";
 
 export default function Field({
     count,
@@ -15,28 +16,84 @@ export default function Field({
     setEndEditorIsActive,
     processIsActive
 }) {
+    const canvasRef = useRef();
+
+    // Состояния для работы с Canvas
+    const [canvas, setCanvas] = useState();
+    const [ctx, setCtx] = useState();
+
+    // Картинки для поля
+    const [images, setImages] = useState();
+
+    // Создать Canvas и загрузить картинки
+    useEffect(() => {
+        createCanvas({
+            canvasRef,
+            setCanvas,
+            setCtx,
+            size: 1000
+        });
+
+        let imagesUploaded = 0;
+        const images = [];
+
+        for (let i = 0; i < 6; i++) {
+            const image = new Image();
+
+            switch (i) {
+                case 0:
+                    image.src = "/grass.png";
+                    break;
+                case 1:
+                    image.src = "/bedrock.png";
+                    break;
+                case 2:
+                    image.src = "/steve.png";
+                    break;
+                case 3:
+                    image.src = "/diamond.png";
+                    break;
+                case 4:
+                    image.src = "/water.png";
+                    break;
+                case 5:
+                    image.src = "/gold.png";
+            }
+
+            images.push(image);
+
+            image.onload = () => {
+                imagesUploaded++;
+    
+                if (imagesUploaded === 6) {
+                    setImages(images);
+                }
+            };
+        }
+    }, []);
+
+    // Canvas загружен, нужно рассчитать функции и координаты
+    useEffect(() => {
+        if (!ctx || !images) return;
+
+        const border = 1000 / count;
+
+        field.forEach((line, row) => line.forEach((cell, column) => {
+            cell.x = border * row;
+            cell.y = border * column;
+
+            cell.changeType = (type) => {
+                ctx.clearRect(cell.x, cell.y, border, border);
+                ctx.drawImage(images[type], cell.x, cell.y, border, border);
+            };
+
+            cell.changeType(cell.type);
+        }));
+    }, [images, field]);
+
     return (
         <div className={style.field}>
-            {field.map((line, row) => line.map((cell, column) => (
-                <Cell
-                    key={(row + 1) * column}
-                    row={row}
-                    column={column}
-                    cell={cell}
-                    count={count}
-                    field={field}
-                    setField={setField}
-                    startCell={startCell}
-                    setStartCell={setStartCell}
-                    endCell={endCell}
-                    setEndCell={setEndCell}
-                    startEditorIsActive={startEditorIsActive}
-                    setStartEditorIsActive={setStartEditorIsActive}
-                    endEditorIsActive={endEditorIsActive}
-                    setEndEditorIsActive={setEndEditorIsActive}
-                    processIsActive={processIsActive}
-                />
-            )))}
+            <canvas id="minecraft" ref={canvasRef} />
         </div>
     );
 }
--- components/Algorithms/Maze/Utils/field.js
+++ components/Algorithms/Maze/Utils/field.js
@@ -1,153 +1,139 @@
 import { sleep } from "../../../../utils/sleep";
 
+// Проверить, не выходим ли за границы поля
+export function cellIsExists(row, column, count) {
+    return 0 <= row && row < count && 0 <= column && column < count;
+}
+
 // Получить поле размером N x N
 export function getField(count) {
-    const field = new Array(count).fill(1).map(() => new Array(count).fill(1));
+    const field = [];
 
-    let y = 0;
-    let x = 0;
+    for (let row = 0; row < count; row++) {
+        const line = [];
 
-    if (field[x] && field[x][y]) {
-        field[x][y] = 0;
-    }
-    
-    
-    let toCheck = [];
-    
-    if (y - 2 >= 0) {
-        toCheck.push({ x, y: y - 2 });
-    }
-
-    if (y + 2 < count) {
-        toCheck.push({ x, y: y + 2 });
-    }
-
-    if (x - 2 >= 0) {
-        toCheck.push({ x: x - 2, y });
-    }
-
-    if (x + 2 < count) {
-        toCheck.push({ x: x + 2, y });
-    }
-  
-    while (toCheck.length > 0) {
-        let index = Math.floor(Math.random() * toCheck.length);
-        let cell = toCheck[index];
-
-        x = cell.x;
-        y = cell.y;
-        
-        if (field[x] && field[x][y]) {
-            field[x][y] = 0;
+        for (let column = 0; column < count; column++) {
+            line.push({
+                row,
+                column,
+                type: 1
+            });
         }
-      
+
+        field.push(line);
+    }
+
+    const startCell = field?.[0]?.[0];
+    const endCell = field?.[count - 1]?.[count - 1];
+
+    if (startCell) startCell.type = 0;
+
+    const toCheck = [];
+
+    if (cellIsExists(0, 2, count)) {
+        toCheck.push(field[0][2]);
+    }
+    if (cellIsExists(2, 0, count)) {
+        toCheck.push(field[2][0]);
+    }
+
+    while (toCheck.length) {
+        const index = Math.floor(Math.random() * toCheck.length);
+        const cell = toCheck[index];
+
+        cell.type = 0;
+
         toCheck.splice(index, 1);
 
-        let directions = ["NORTH", "SOUTH", "EAST", "WEST"];
-        while (directions.length > 0) {
-            let dirIndex = Math.floor(Math.random() * directions.length);
-        
-            switch (directions[dirIndex]) {
+        const directions = ["NORTH", "SOUTH", "EAST", "WEST"];
+        const { row, column } = cell;
+        let barrierIsPlaced = false;
+
+        while (!barrierIsPlaced) {
+            const directionIndex = Math.floor(Math.random() * directions.length);
+            const direction = directions[directionIndex];
+
+            switch (direction) {
                 case "NORTH":
-                    if (y - 2 >= 0 && field[x][y - 2] == 0) {
-                        field[x][y - 1] = 0;
-                        directions = [];
+                    if (column >= 2 && field[row][column - 2].type === 0) {
+                        field[row][column - 1].type = 0;
+                        barrierIsPlaced = true;
                     }
                     break;
-              
                 case "SOUTH":
-                    if (y + 2 < count && field[x][y + 2] == 0) {
-                        field[x][y + 1] = 0;
-                        directions = [];
+                    if (column + 2 < count && field[row][column + 2].type === 0) {
+                        field[row][column + 1].type = 0;
+                        barrierIsPlaced = true;
                     }
                     break;
-
                 case "EAST":
-                    if (x - 2 >= 0 && field[x - 2][y] == 0) {
-                        field[x - 1][y] = 0;
-                        directions = [];
+                    if (row >= 2 && field[row - 2][column].type === 0) {
+                        field[row - 1][column].type = 0;
+                        barrierIsPlaced = true;
                     }
                     break;
-
                 case "WEST":
-                    if (x + 2 < count && field[x + 2][y] == 0) {
-                        field[x + 1][y] = 0;
-                        directions = [];
+                    if (row + 2 < count && field[row + 2][column].type === 0) {
+                        field[row + 1][column].type = 0;
+                        barrierIsPlaced = true;
                     }
-                    break;
             }
 
-            directions.splice(dirIndex, 1);
+            directions.splice(directionIndex, 1);
         }
 
-        if (y - 2 >= 0 && field[x][y - 2] == 1) {
-            toCheck.push({ x, y: y - 2 });
+        if (column >= 2 && field[row][column - 2] === 1) {
+            toCheck.push({ row, column: column - 2 });
         }
-
-        if (y + 2 < count && field[x][y + 2] == 1) {
-            toCheck.push({ x, y: y + 2 });
+        if (column + 2 < count && field[row][column + 2] === 1) {
+            toCheck.push({ row, column: column + 2 });
         }
-
-        if (x - 2 >= 0 && field[x - 2][y] == 1) {
-            toCheck.push({ x: x - 2, y });
+        if (row >= 2 && field[row - 2][column] === 1) {
+            toCheck.push({ row: row - 2, column });
         }
-
-        if (x + 2 < count && field[x + 2][y] == 1) {
-            toCheck.push({ x: x + 2, y });
+        if (row + 2 < count && field[row + 2][column] === 1) {
+            toCheck.push({ row: row + 2, column });
         }
     }
 
     for (let i = 0; i < 2; i++) {
-        let dead_ends = [];
-        
+        const deadEnds = [];
+
         for (let column = 0; column < count; column++) {
-            for (let row = 0; row < count ; row++) {  
-                if (field[row][column] == 0) {
-                  let neighbors = 0;
-                        
-                    if (column - 1 >= 0 && field[row][column - 1] == 0) {
-                        neighbors++;
-                    }
+            for (let row = 0; row < count; row++) {  
+                if (field[row][column] === 0) {
+                    const neighboursCount = (
+                        column >= 1 && field[row][column - 1] === 0
+                    ) + (
+                        column + 1 < count && field[row][column + 1] === 0
+                    ) + (
+                        column + 1 < count && field[row][column + 1] === 0
+                    ) + (
+                        row + 1 < count && field[row + 1][column] === 0
+                    );
 
-                    if (column + 1 < count && field[row][column + 1] == 0) {
-                        neighbors++;
-                    }
-
-                    if (row - 1 >= 0 && field[row - 1][column] == 0) {
-                        neighbors++;
-                    }
-
-                    if (row + 1 < count && field[row + 1][column] == 0) {
-                        neighbors++;
-                    }
-
-                    if (neighbors <= 1) {
-                        dead_ends.push({x:row, y:column});
+                    if (neighboursCount <= 1) {
+                        deadEnds.push(field[row][column]);
                     }
                 }
             }
         }
-    
-        for(let cell of dead_ends) {
-            field[cell.x][cell.y] = 1;
+
+        for (const cell of deadEnds) {
+            cell.type = 1;
         }
     }  
 
-    if (count != 0) {
-        field[0][0] = 2;
-    }
-
-    if (count - 1 >= 0) {
-        field[count - 1][count - 1] = 3;
-    }
+    if (startCell) startCell.type = 2;
+    if (endCell) endCell.type = 3;
 
     for (let i = 0; i < 3 && i < count - 1; i++) { 
-        for (let j = 0; j < 3 && j < count - 1;j++) {
-            if (field[i][j] == 1) {
+        for (let j = 0; j < 3 && j < count - 1; j++) {
+            if (field[i][j] === 1) {
                 field[i][j] = 0;
-            } else if (field[j][i] == 1) {
+            } else if (field[j][i] === 1) {
                 field[j][i] = 0;
-            } else { 
+            } else {
                 break;
             }
         }
@@ -155,26 +141,20 @@ export function getField(count) {
 
     for (let i = count - 1; i >= 0 && i > count - 4; i--) { 
         for (let j = count - 1; j >= 0 && j > count - 4; j--) {
-            if (field[i][j] == 1) {
+            if (field[i][j] === 1) {
                 field[i][j] = 0;
-            } else if (field[j][i] == 1) {
+            } else if (field[j][i] === 1) {
                 field[j][i] = 0;
             } else { 
                 break;
             }
         }
-    } 
+    }
 
     return {
         field,
-        startCell: {
-            row: 0,
-            column: 0
-        },
-        endCell: {
-            row: count - 1,
-            column: count - 1
-        }
+        startCell,
+        endCell
     };
 }
 
@@ -188,11 +168,6 @@ export function getExtendedField(field) {
             move: -1
         }));
     });
-}
-
-// Проверить, не выходим ли за границы поля
-export function cellIsExists(row, column, count) {
-    return 0 <= row && row < count && 0 <= column && column < count;
 }
 
 // Получить следующую итерацию
