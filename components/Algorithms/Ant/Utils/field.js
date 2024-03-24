45888e5d5a437c47dcdc9f569da7fb2a7b8a0b42
yazloysasha <yazloysasha@gmail.com>
2024-03-24 22:56:56 +0700

    Добавлено целенаправленное перемещение муравьёв

--- components/Algorithms/Ant/Utils/field.js
+++ components/Algorithms/Ant/Utils/field.js
@@ -1,4 +1,12 @@
-import { getRandomElement, sleep } from "../../../../utils/helpers";
+import { getRandomElement } from "../../../../utils/helpers";
+
+// Стороны квадрата
+const sides = [
+    { x: 0, y: 0, delta: { x: false, y: true } },
+    { x: 0, y: 1, delta: { x: true, y: false } },
+    { x: 1, y: 0, delta: { x: true, y: false } },
+    { x: 1, y: 1, delta: { x: false, y: true } }
+];
 
 // Проверить, не выходим ли за границы поля
 export function cellIsExists(row, column, count) {
@@ -61,7 +69,12 @@ export function getAnts(field, colonyCell, population) {
         ants.push({
             cell: colonyCell,
             leftIndent: Math.random() * 100,
-            topIndent: Math.random() * 100
+            topIndent: Math.random() * 100,
+            position: {
+                x: 0.5,
+                y: 0.5
+            },
+            look: Math.random() * Math.PI * 2
         });
     }
 
@@ -113,34 +126,123 @@ export function getNextCell(i, { row, column }) {
     return { row, column };
 }
 
+// Получить позицию взгляда в квадрате по его углу
+export function getPositionOnSide(position, look) {
+    const k = Math.tan(look);
+    const b = position.y - k * position.x;
+
+    return sides.map((side) => {
+        // Вертикальная сторона
+        if (!side.delta.x) {
+            return {
+                x: side.x,
+                y: k * side.x + b
+            };
+        }
+
+        // Горизонтальная сторона
+        if (!side.delta.y) {
+            return {
+                x: (side.y - b) / k,
+                y: side.y
+            };
+        }
+    }).filter((positionOnSide) => {
+        const { x, y } = positionOnSide;
+
+        return (
+            x !== position.x && y !== position.y
+        ) && (
+            0 <= x && x <= 1 && 0 <= y && y <= 1
+        ) && (
+            position.x !== 0.5 || position.y !== 0.5 || (
+                x === 0 && (
+                    look >= Math.PI / 2 && look < Math.PI
+                ) || x === 1 && (
+                    look < Math.PI / 2 || look >= Math.PI
+                ) || y === 0 && (
+                    look >= Math.PI
+                ) || y === 1 && (
+                    look < Math.PI
+                )
+            )
+        );
+    })[0];
+}
+
 // Одна итерация муравьиной колонии
-export async function runColony({
+export function runColony({
     count,
     field,
     ants,
     setAnts
 }) {
     ants.forEach((ant) => {
-        const neighboringCells = [];
+        let availableCellsCount = 0;
 
+        // Проверить, не замурован ли муравей
         for (let i = 0; i < 4; i++) {
             const { row, column } = getNextCell(i, ant.cell);
 
             if (!cellIsExists(row, column, count)) continue;
 
             if (field[row][column].type !== 1) {
-                neighboringCells.push(field[row][column]);
+                availableCellsCount++;
             }
         }
 
-        if (neighboringCells.length) {
-            ant.cell.ants--;
-            ant.cell = getRandomElement(neighboringCells);
-            ant.cell.ants++;
+        // Если муравей замурован, то ничего не делать
+        if (!availableCellsCount) return;
+
+        // Если муравей в тупике, то развернуть его
+        if (availableCellsCount === 1) {
+            ant.position.x = Math.abs(ant.position.x - 1);
+            ant.position.y = Math.abs(ant.position.y - 1);
         }
+
+        // С вероятностью 30% изменить взгляд муравья ("передумал")
+        if (Math.random < 0.3) {
+            ant.look = Math.random() * Math.PI * 2;
+        }
+
+        let nextStep;
+
+        // Найти ячейку и позицию для следующего шага муравья
+        while (!nextStep) {
+            let { x, y } = getPositionOnSide(ant.position, ant.look);
+            let { row, column } = ant.cell;
+
+            if (x === 0) {
+                row--;
+                x = 1;
+            } else if (x === 1) {
+                row++;
+                x = 0;
+            } else if (y === 0) {
+                column--;
+                y = 1;
+            } else if (y === 1) {
+                column++;
+                y = 0;
+            }
+
+            if (cellIsExists(row, column, count) && field[row][column].type !== 1) {
+                nextStep = {
+                    cell: field[row][column],
+                    position: { x, y }
+                };
+            } else {
+                // Изменить взгляд муравья (имитация ударения об стенку)
+                ant.look = Math.random() * Math.PI * 2;
+            }
+        }
+
+        ant.cell.ants--;
+        ant.cell = nextStep.cell;
+        ant.cell.ants++;
+
+        ant.position = nextStep.position;
     });
 
     setAnts([...ants]);
-
-    await sleep(200);
 }
