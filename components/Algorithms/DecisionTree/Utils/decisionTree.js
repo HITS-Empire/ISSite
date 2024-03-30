d65095079f046647b5546a2e200cd66dad73f1aa
yazloysasha <yazloysasha@gmail.com>
2024-03-30 19:06:34 +0700

    Добавлен алгоритм работы с деревом решений

---
+++ components/Algorithms/DecisionTree/Utils/decisionTree.js
@@ -0,0 +1,185 @@
+// Доступные предикаты
+const predicates = {
+    "==": (a, b) => a == b,
+    ">=": (a, b) => a >= b
+};
+
+// Предсказать событие
+export function predict(decisionTree, field) {
+    let attribute, value, predicate, pivot;
+
+    while (true) {
+        if (decisionTree.category) {
+            return decisionTree.category;
+        }
+
+        attribute = decisionTree.attribute;
+        value = field[attribute];
+
+        predicate = decisionTree.predicate;
+        pivot = decisionTree.pivot;
+
+        if (predicate(value, pivot)) {
+            decisionTree = decisionTree.match;
+        } else {
+            decisionTree = decisionTree.notMatch;
+        }
+    }
+}
+
+// Получить количество уникальных значений
+export function getCountOfUniqueValues(set, attribute) {
+    const counter = {};
+
+    for (let i = set.length - 1; i >= 0; i--) {
+        counter[set[i][attribute]] = 0;
+    }
+
+    for (let i = set.length - 1; i >= 0; i--) {
+        counter[set[i][attribute]]++;
+    }
+
+    return counter;
+}
+
+// Получить наиболее частое совпадение
+export function getMostFrequentValue(set, attribute) {
+    const counter = getCountOfUniqueValues(set, attribute);
+
+    let mostFrequentValue, mostFrequentCount = 0;
+
+    for (const value in counter) {
+        if (counter[value] > mostFrequentCount) {
+            mostFrequentCount = counter[value];
+            mostFrequentValue = value;
+        }
+    };
+
+    return mostFrequentValue;
+}
+
+// Получить значение энтропии
+export function getEntropy(set, attribute) {
+    const counter = getCountOfUniqueValues(set, attribute);
+
+    let p, entropy = 0;
+    for (const i in counter) {
+        p = counter[i] / set.length;
+        entropy += -p * Math.log(p);
+    }
+
+    return entropy;
+}
+
+// Получить разделение дерева
+export function getSplit(set, attribute, predicate, pivot) {
+    const match = [], notMatch = [];
+      
+    for (var i = set.length - 1; i >= 0; i--) {
+        const field = set[i];
+        const attributeValue = field[attribute];
+
+        if (predicate(attributeValue, pivot)) {
+            match.push(field);
+        } else {
+            notMatch.push(field);
+        }
+    };
+
+    return { match, notMatch };
+}
+
+// Получить дерево решений на основе выборки
+export function getDecisionTree({
+    trainingSet,
+    requiredAttribute,
+    maxDepth = 128
+}) {
+    const minItemsCount = 1;
+    const entropyThrehold = 0.01;
+
+    if (maxDepth === 0 || trainingSet.length <= minItemsCount) {
+        return {
+            category: getMostFrequentValue(trainingSet, requiredAttribute)
+        };
+    }
+
+    let initialEntropy = getEntropy(trainingSet, requiredAttribute);
+
+    if (initialEntropy <= entropyThrehold) {
+        return {
+            category: getMostFrequentValue(trainingSet, requiredAttribute)
+        };
+    }
+
+    const alreadyChecked = {};
+    let bestSplit = { gain: 0 };
+
+    for (let i = trainingSet.length - 1; i >= 0; i--) {
+        const field = trainingSet[i];
+
+        for (let attribute in field) {
+            if (attribute === requiredAttribute) continue;
+
+            const pivot = field[attribute];
+            const predicateName = typeof pivot === "number" ? ">=" : "==";
+            const attributePredicatePivot = attribute + predicateName + pivot;
+
+            if (alreadyChecked[attributePredicatePivot]) continue;
+
+            alreadyChecked[attributePredicatePivot] = true;
+
+            const predicate = predicates[predicateName];
+            const currentSplit = getSplit(trainingSet, attribute, predicate, pivot);
+
+            const matchEntropy = getEntropy(currentSplit.match, requiredAttribute);
+            const notMatchEntropy = getEntropy(currentSplit.notMatch, requiredAttribute);
+
+            let newEntropy = 0;
+            newEntropy += matchEntropy * currentSplit.match.length;
+            newEntropy += notMatchEntropy * currentSplit.notMatch.length;
+            newEntropy /= trainingSet.length;
+
+            const currentGain = initialEntropy - newEntropy;
+            if (currentGain > bestSplit.gain) {
+                bestSplit = currentSplit;
+                bestSplit.predicateName = predicateName;
+                bestSplit.predicate = predicate;
+                bestSplit.attribute = attribute;
+                bestSplit.pivot = pivot;
+                bestSplit.gain = currentGain;
+            }
+        }
+    }
+
+    if (!bestSplit.gain) {
+        return {
+            category: getMostFrequentValue(trainingSet, requiredAttribute)
+        };
+    }
+
+    maxDepth--;
+
+    // Рекурсивно заполняем деревья-потомки
+    const matchSubTree = getDecisionTree({
+        trainingSet: bestSplit.match,
+        requiredAttribute,
+        maxDepth
+    });
+    const notMatchSubTree = getDecisionTree({
+        trainingSet: bestSplit.notMatch,
+        requiredAttribute,
+        maxDepth
+    });
+
+    return {
+        attribute: bestSplit.attribute,
+        predicate: bestSplit.predicate,
+        predicateName: bestSplit.predicateName,
+        pivot: bestSplit.pivot,
+        match: matchSubTree,
+        notMatch: notMatchSubTree,
+        matchedCount: bestSplit.match.length,
+        notMatchedCount: bestSplit.notMatch.length
+    };
+}
--- pages/derevo.js
+++ pages/derevo.js
@@ -1,6 +1,31 @@
+import {
+    predict,
+    getDecisionTree
+} from "../components/Algorithms/DecisionTree/Utils/decisionTree";
 import Component from "../components/Algorithms/DecisionTree/Component";
 
 export default function DecisionTree() {
+    const trainingSet = [
+        { hairLength: 0, weight: 250, age: 36, sex: "male" },
+        { hairLength: 10, weight: 150, age: 34, sex: "female" },
+        { hairLength: 2, weight: 90, age: 10, sex: "male" },
+        { hairLength: 6, weight: 78, age: 8, sex: "female" },
+        { hairLength: 4, weight: 20, age: 1, sex: "female" },
+        { hairLength: 1, weight: 170, age: 70, sex: "male" },
+        { hairLength: 8, weight: 160, age: 41, sex: "female" },
+        { hairLength: 10, weight: 180, age: 38, sex: "male" },
+        { hairLength: 6, weight: 200, age: 45, sex: "male" }
+    ];
+    const requiredAttribute = "sex";
+
+    const decisionTree = getDecisionTree({ trainingSet, requiredAttribute });
+
+    const result = predict(
+        decisionTree,
+        { hairLength: 80, weight: 90, age: 38 }
+    );
+    console.log(result);
+
     return (
         <Component />
     );
