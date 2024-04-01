56dddd79a97a989fc0f546ebf37e70022b4b46e7
yazloysasha <yazloysasha@gmail.com>
2024-04-01 13:48:07 +0700

    Сделано перемещение дерева

--- components/Algorithms/DecisionTree/Screen/index.js
+++ components/Algorithms/DecisionTree/Screen/index.js
@@ -1,8 +1,44 @@
 import style from "./style.module.scss";
+import { useState, useEffect } from "react";
 
 export default function Screen({
     decisionTree
 }) {
+    // Стартовая точка перемещения
+    const [moveScreenStartPoint, setMoveScreenStartPoint] = useState();
+
+    // Отступы для перемещения
+    const [marginTop, setMarginTop] = useState(0);
+    const [marginLeft, setMarginLeft] = useState(0);
+
+    // Сохранённые отступы
+    const [savedMarginTop, setSavedMarginTop] = useState(0);
+    const [savedMarginLeft, setSavedMarginLeft] = useState(0);
+
+    // Начать перемещение экрана
+    const moveScreenStart = (event) => {
+        const { offsetX, offsetY } = event.nativeEvent;
+
+        setMoveScreenStartPoint({ x: offsetX, y: offsetY });
+    };
+
+    // Закончить перемещение экрана
+    const moveScreenEnd = () => {
+        setMoveScreenStartPoint();
+        setSavedMarginTop(marginTop);
+        setSavedMarginLeft(marginLeft);
+    };
+
+    // Само перемещение экрана
+    const moveScreen = (event) => {
+        if (!moveScreenStartPoint) return;
+
+        const { offsetX, offsetY } = event.nativeEvent;
+
+        setMarginTop(savedMarginTop + offsetY - moveScreenStartPoint.y);
+        setMarginLeft(savedMarginLeft + offsetX - moveScreenStartPoint.x);
+    };
+
     // Рекурсивное преобразование дерева в HTML-граф
     const getDecisionTreeComponent = (decisionNode) => {
         if (decisionNode.category) {
@@ -40,12 +76,34 @@ export default function Screen({
         );
     };
 
+    // Сбросить состояния поля при перезагрузке дерева
+    useEffect(() => {
+        if (decisionTree) return;
+
+        setMarginLeft(0);
+        setMarginTop(0);
+        setSavedMarginLeft(0);
+        setSavedMarginTop(0);
+    }, [decisionTree]);
+
     // Визуализация дерева решений
     const decisionTreeComponent = decisionTree && getDecisionTreeComponent(decisionTree);
 
     return (
-        <div className={style.screenContainer}>
-            <div className={style.screen}>
+        <div className={style.screen}>
+            <div
+                className={style.handler}
+                onMouseDown={moveScreenStart}
+                onMouseUp={moveScreenEnd}
+                onMouseOut={moveScreenEnd}
+                onMouseMove={moveScreen}
+            />
+            <div
+                className={style.tree}
+                style={{
+                    margin: `${marginTop}px 0 0 ${marginLeft}px`
+                }}
+            >
                 {decisionTreeComponent}
             </div>
         </div>
