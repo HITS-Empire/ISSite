bc1eabfbc4589d3ff7cced08f2de5daa9bd86068
yazloysasha <yazloysasha@gmail.com>
2024-04-09 00:45:41 +0700

    Создана основа для бонусной генетики

--- components/Algorithms/Genetic/Bonus/Menu/index.js
+++ components/Algorithms/Genetic/Bonus/Menu/index.js
@@ -1,10 +1,30 @@
+import style from "./style.module.scss";
+import Button from "../../../../Button";
 import MenuWrapper from "../../../../MenuWrapper";
+import ButtonContainer from "../../../../ButtonContainer";
 
 export default function Menu() {
     return (
         <MenuWrapper
+            className={style.menu}
             title="Генетический алгоритм"
-            description="Сейчас вы узрите, как работает бонусный генетический алгоритм."
-        />
+            description="Сейчас вы узрите, как выполняется генерация кода для вывода последовательности Фибоначчи."
+        >
+            <ButtonContainer>
+                <Button
+                    type="primary"
+                    onClick={() => {}}
+                >
+                    Запустить алгоритм
+                </Button>
+
+                <Button
+                    type="soft"
+                    onClick={() => {}}
+                >
+                    Отменить
+                </Button>
+            </ButtonContainer>
+        </MenuWrapper>
     );
 }
