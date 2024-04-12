5218a3a2a4cfc3181241f8757333a46bec390585
yazloysasha <yazloysasha@gmail.com>
2024-04-12 12:09:53 +0700

    Добавлено лого

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,18 @@
+import Head from "next/head";
 import MainLayout from "../components/MainLayout";
 
 import "../styles/_globals.scss";
 
 export default function App({ Component, pageProps }) {
     return (
-        <MainLayout>
-            <Component {...pageProps} />
-        </MainLayout>
+        <>
+            <Head>
+                <link rel="icon" href="gte.png" type="image/png" />
+            </Head>
+
+            <MainLayout>
+                <Component {...pageProps} />
+            </MainLayout>
+        </>
     );
 }
